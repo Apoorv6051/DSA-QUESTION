15901810c4485ae88a02276c7edad70bbbecada0
Apoorv6051 <192407348+Apoorv6051@users.noreply.github.com>
2026-07-15 22:55:25 +0530

    Time: 1 ms (83.11%), Space: 42.8 MB (20.94%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,7 +1,19 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n;
+       int evenSum =((n*n)+n);
+       int  oddsum = (n*n); 
 
-        
+       // after that we will use Euclidean algorithm to find gcd
+       int b= evenSum;
+       int a =oddsum;
+       while(b !=0){
+        int temp = b;
+        b= a % b;
+        a= temp;
+
+       }
+       return a;
+
+       
     }
 }
