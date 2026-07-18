01e583936a14c9319b359cea0be3f323c58542e5
Apoorv6051 <192407348+Apoorv6051@users.noreply.github.com>
2026-07-19 00:50:27 +0530

    Time: 0 ms (100%), Space: 45.2 MB (28.11%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,23 +1,25 @@
 class Solution {
     public int findGCD(int[] nums) {
-        int mn = 1001, mx = 0;
-
-        for (int num : nums) {
-            if (num < mn)
-                mn = num;
-            if (num > mx)
-                mx = num;
+        // BRUTE FORCE SOLUTION OF THIS
+        int max=0;
+        int min =nums[0];
+        int a ,b;
+        int n= nums.length;
+        for(int i =0;i<n;i++){
+        max=Math.max(max,nums[i]);
+        min = Math.min(min,nums[i]);
         }
+        // APPLYING EUCLIDEAN ALGO
+    a=max;
+    b=min;
+    while(b!=0){
+        int temp =b;
+        b = a % b;
+        a=temp;
 
-        return gcd(mn, mx);
     }
 
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int rem = a % b;
-            a = b;
-            b = rem;
-        }
+
         return a;
     }
 }
