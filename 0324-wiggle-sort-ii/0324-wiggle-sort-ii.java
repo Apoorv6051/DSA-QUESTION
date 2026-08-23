8859473a3fa674bffbd079c5c8d633bf8e22e510
Apoorv pal <2k24.csai1a.2413804@gmail.com>
2026-08-23 20:46:31 +0530

    Time: 1 ms (100%), Space: 50 MB (5.31%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,14 +1,29 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        int n = nums.length-1;
-        int[] arr = Arrays.copyOf(nums,nums.length);
-        Arrays.sort(arr);
-        for(int i=1;i<nums.length;i+=2){
-            nums[i]=arr[n--];
+        // Step 1: Create a frequency map. Since max value is 5000, size 5001 is enough.
+        int[] count = new int[5001];
+        for (int num : nums) {
+            count[num]++;
         }
-        for(int i=0;i<nums.length;i+=2){
-            nums[i]=arr[n--];
+        
+        int max_val = 5000;
+        
+        // Step 2: Fill the Peaks (odd indices) with the largest available numbers.
+        for (int i = 1; i < nums.length; i += 2) {
+            while (count[max_val] == 0) {
+                max_val--;
+            }
+            nums[i] = max_val;
+            count[max_val]--;
         }
-
+        
+        // Step 3: Fill the Valleys (even indices) with the remaining smaller numbers.
+        for (int i = 0; i < nums.length; i += 2) {
+            while (count[max_val] == 0) {
+                max_val--;
+            }
+            nums[i] = max_val;
+            count[max_val]--;
+        }
     }
 }
