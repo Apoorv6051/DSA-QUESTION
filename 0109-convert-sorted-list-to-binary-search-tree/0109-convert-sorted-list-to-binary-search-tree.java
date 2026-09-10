fee0548931736bdcc9ebd89b1fc7525f06c229f1
Apoorv pal <2k24.csai1a.2413804@gmail.com>
2026-09-10 12:31:54 +0530

    Time: 1 ms (19.81%), Space: 47.1 MB (20.58%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -1,20 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public TreeNode sortedListToBST(ListNode head) {
-        if(head==null)
+    public TreeNode build(List<Integer> ele , int s, int e){
+        if(s>e){
             return null;
-        if(head.next==null)
-            return new TreeNode(head.val);
-        ListNode slow=head;
-        ListNode fast=head.next.next;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
         }
-        TreeNode res=new TreeNode(slow.next.val);
-        ListNode righthalf=slow.next.next;
-        slow.next=null;
-        res.left=sortedListToBST(head);
-        res.right=sortedListToBST(righthalf);
-        return res;
+        int mid = s+(e-s)/2;
+        TreeNode root = new TreeNode(ele.get(mid)); // tsking middle element as node
+        root.left = build(ele,s,mid-1);
+        root.right=build(ele,mid+1,e);
+
+        return root;
+
+    }
+    public TreeNode sortedListToBST(ListNode head) {// putting value of linked lisst to arrayList
+        List<Integer> ele = new ArrayList<>();
+        while(head != null){
+            ele.add(head.val);
+            head = head.next;
+        }
+        return build(ele,0,ele.size()-1);
+
+        
+        
     }
 }
