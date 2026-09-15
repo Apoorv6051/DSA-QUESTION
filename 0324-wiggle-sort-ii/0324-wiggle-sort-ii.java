4953de2a229b48d23208a3421147db4023ad830f
Apoorv pal <2k24.csai1a.2413804@gmail.com>
2026-09-15 15:06:31 +0530

    Time: 98 ms (5.07%), Space: 49.8 MB (5.38%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,29 +1,91 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        // Step 1: Create a frequency map. Since max value is 5000, size 5001 is enough.
-        int[] count = new int[5001];
-        for (int num : nums) {
-            count[num]++;
-        }
-        
-        int max_val = 5000;
-        
-        // Step 2: Fill the Peaks (odd indices) with the largest available numbers.
-        for (int i = 1; i < nums.length; i += 2) {
-            while (count[max_val] == 0) {
-                max_val--;
+        int n = nums.length;
+
+        // 1. Find median using QuickSelect
+        int median = quickSelect(nums, n / 2);
+
+        // 2. 3-way partition with virtual indexing
+        int left = 0;
+        int i = 0;
+        int right = n - 1;
+
+        while (i <= right) {
+
+            int mappedI = newIndex(i, n);
+
+            if (nums[mappedI] > median) {
+
+                swap(nums, newIndex(left, n), mappedI);
+                left++;
+                i++;
+
+            } else if (nums[mappedI] < median) {
+
+                swap(nums, mappedI, newIndex(right, n));
+                right--;
+
+            } else {
+                i++;
             }
-            nums[i] = max_val;
-            count[max_val]--;
         }
-        
-        // Step 3: Fill the Valleys (even indices) with the remaining smaller numbers.
-        for (int i = 0; i < nums.length; i += 2) {
-            while (count[max_val] == 0) {
-                max_val--;
+    }
+
+    // Virtual indexing:
+    // 0,1,2,3,4,5 -> 1,3,5,0,2,4
+    private int newIndex(int i, int n) {
+        return (1 + 2 * i) % (n | 1);
+    }
+
+
+    // QuickSelect
+    private int quickSelect(int[] nums, int k) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int pivotIndex = partition(nums, left, right);
+
+            if (pivotIndex == k) {
+                return nums[pivotIndex];
             }
-            nums[i] = max_val;
-            count[max_val]--;
+
+            if (pivotIndex < k) {
+                left = pivotIndex + 1;
+            } else {
+                right = pivotIndex - 1;
+            }
         }
+
+        return -1;
+    }
+
+
+    // Lomuto partition
+    private int partition(int[] nums, int left, int right) {
+
+        int pivot = nums[right];
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+
+            if (nums[j] <= pivot) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+
+        swap(nums, i, right);
+
+        return i;
+    }
+
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
